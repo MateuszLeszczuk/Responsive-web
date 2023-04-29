8f7c11e27d8e4bbde381c63a85fa115feae80cb7
Mateusz <leszczukpriv@gmail.com>
2023-04-29 16:29:22 +0200

    completed planets section & repared nav mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 
+const OptionCard = document.querySelector('#oCard');
+const OptionPlanets = document.querySelector('#oPlanets');
+const OptionContact = document.querySelector('#oContact');
+
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active');
+    navMobile.classList.toggle('nav-mobile--active'); console.log(navMobileOption);
+}
+
+const hideNav = () => {
+    navBtn.classList.remove('is-active');
+    navMobile.classList.remove('nav-mobile--active');
+
+
+
 }
 
 
 navBtn.addEventListener('click', handleNav);
+
+OptionCard.addEventListener('click', hideNav);
+OptionPlanets.addEventListener('click', hideNav);
+OptionContact.addEventListener('click', hideNav);
+
 
