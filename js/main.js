ad2fb18877c42b0e41cd36dd9f5f5f19c4c80c19
Mateusz <leszczukpriv@gmail.com>
2023-05-04 11:43:54 +0200

    improving javascript and small changes in html,scss

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
+const allNavLinks = document.querySelectorAll('.nav__link');
 
-const OptionCard = document.querySelector('#oCard');
-const OptionPlanets = document.querySelector('#oPlanets');
-const OptionContact = document.querySelector('#oContact');
 
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active'); console.log(navMobileOption);
+    navMobile.classList.toggle('nav-mobile--active');
+
+    allNavLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            navBtn.classList.remove('is-active');
+            navMobile.classList.remove('nav-mobile--active');
+        })
+    })
 }
 
-const hideNav = () => {
-    navBtn.classList.remove('is-active');
-    navMobile.classList.remove('nav-mobile--active');
-
-
-
-}
 
 
 navBtn.addEventListener('click', handleNav);
 
-OptionCard.addEventListener('click', hideNav);
-OptionPlanets.addEventListener('click', hideNav);
-OptionContact.addEventListener('click', hideNav);
+
 
 
